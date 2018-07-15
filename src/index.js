0d4a57a5f6e6ea0efd75fd4f55f560f5ac620842
dominicom <dominik.kiepuszewski@gmail.com>
2018-07-15 12:34:32 +0200

    deactivating SW

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import './index.css'
 
 
@@ -10,4 +10,4 @@ ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>,
   document.getElementById('root')
 );
-registerServiceWorker();
+//registerServiceWorker();
